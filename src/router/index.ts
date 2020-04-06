4e3c2fe588c1780c7ee7abdbb5428541b9aff521
caoyugang_1 <1442770751@qq.com>
2020-04-06 11:17:00 +0800

    feat:init vue build

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
+
+Vue.use(VueRouter)
+// tslint:disable-next-line: array-type
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
